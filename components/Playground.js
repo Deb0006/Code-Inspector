5bde754ec3e5e58299f96d4aeed4ae40af66c231
Debby <34969423+Deb0006@users.noreply.github.com>
2022-06-27 15:59:32 -0400

    Added form layout

--- components/Playground.js
+++ components/Playground.js
@@ -1,6 +1,22 @@
-import "../styles/globals.css";
+import "../styles/Navbar.module.css";
 const Playground = () => {
-  return <section></section>;
+  return (
+    <section>
+      <h2>Ask OpenAI to brainstorm ideas combining two terms</h2>
+      <form>
+        <label>
+          Type an idea e.g sports
+          <input></input>
+        </label>
+        <label>
+          Type another idea e.g apples
+          <input></input>
+        </label>
+        <button></button>
+        <div></div>
+      </form>
+    </section>
+  );
 };
 
 export default Playground;
