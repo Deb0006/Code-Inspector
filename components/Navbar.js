3b496b3abc786a79ec41f563ea999ae6ac615611
Debby <34969423+Deb0006@users.noreply.github.com>
2022-07-02 15:03:03 -0400

    Updates: hamburger icon and background image
    hamburger icon and dropdown menu fully working now. Navbar background is now a background-image with media queries

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,11 @@
 import styles from "../styles/Navbar.module.css";
-import { FiAlignCenter, FiX } from "react-icons/fi";
+import { useRef, useState } from "react";
+import { Squash as Hamburger } from "hamburger-react";
+// import { FiAlignCenter, FiX } from "react-icons/fi";
+
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+  function fadeOut() {}
   return (
     <nav className={styles.navbar}>
       <img
@@ -14,11 +19,20 @@ const Navbar = () => {
           <br />
           Inspector
         </h1>
-        <a className={styles.description}>Code explained using OpenAI</a>
+        <h2 className={styles.description}>Code explained with OpenAI</h2>
       </div>
       <div className={styles.animatedIcon}>
-        <FiAlignCenter />
-        <FiX />
+        <Hamburger
+          className={styles.hamburger}
+          toggled={isOpen}
+          toggle={setOpen}
+          size={25}
+          label="Show menu"
+        />
+      </div>
+      <div className={isOpen ? styles.dropdownMenu : styles.dropdownMenuHidden}>
+        <a className={styles.menuOptions}>Examples</a>
+        <a className={styles.menuOptions}>About</a>
       </div>
     </nav>
   );
