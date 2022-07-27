456f9db2b9a5befdf8d2ecf454008802f5a4733d
Debby <34969423+Deb0006@users.noreply.github.com>
2022-07-26 23:06:59 -0400

    Added About description

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,13 @@
 const Playground = () => {
   return (
     <section>
-      <h1>*3*</h1>
+      <h1>Code Inspector</h1>
+      <p>
+        This website uses the OPEN-AI API to generate results. Open-ai is a
+        powerful artificial intelligence platform that enables developers to
+        create intelligent applications. For more information, please visit
+        their website.
+      </p>
     </section>
   );
 };
