8075539b05454cefa618d1c2c960755423badc92
Debby <34969423+Deb0006@users.noreply.github.com>
2022-10-02 16:06:24 -0400

    Modified Navbar styles and Sign in and Sign out button

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,46 +9,54 @@ const Navbar = () => {
   const [user] = useAuthState(auth);
   return (
     <nav className={styles.navbar}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
-          Code
-          <br />
-          Inspector
-        </h1>
-        <h2 className={styles.description}>AI-powered website</h2>
-      </div>
-      <div className={styles.animatedIcon}>
-        <label htmlFor="hamburguer" aria-label="Open or close other options">
-          <Hamburger
-            className={styles.hamburger}
-            id="hamburguer"
-            toggled={isOpen}
-            toggle={setOpen}
-            size={26}
-            label="Show menu"
-            rounded
-          />
-        </label>
-      </div>
-      <div className={isOpen ? styles.dropdownMenu : styles.dropdownMenuHidden}>
-        <Link href="/">
-          <span className={styles.menuOptions}>Inspector</span>
-        </Link>
-        <Link href="/results">
-          <span className={styles.menuOptions}>Results</span>
-        </Link>
-        <Link href="/about">
-          <span className={styles.menuOptions}>About</span>
-        </Link>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            Code
+            <br />
+            Inspector
+          </h1>
+          <h2 className={styles.description}>AI-powered website</h2>
+        </div>
         {user ? (
-          <button className={styles.signIn} onClick={() => auth.signOut()}>
-            Sign out
+          <button className={styles.signOut} onClick={() => auth.signOut()}>
+            Sign-Out
           </button>
         ) : (
-          <button className={styles.signOut} onClick={signInWithGoogle}>
-            Sign in
+          <button className={styles.signIn} onClick={signInWithGoogle}>
+            Sign-In
           </button>
         )}
+        <div className={styles.animatedIcon}>
+          <label htmlFor="hamburguer" aria-label="Open or close other options">
+            <Hamburger
+              className={styles.hamburger}
+              id="hamburguer"
+              toggled={isOpen}
+              toggle={setOpen}
+              size={26}
+              label="Show menu"
+              rounded
+            />
+          </label>
+        </div>
+        <div
+          className={
+            isOpen === true || undefined
+              ? styles.dropdownMenu
+              : styles.dropdownMenuHidden
+          }
+        >
+          <Link href="/">
+            <span className={styles.menuOptions}>Inspector</span>
+          </Link>
+          <Link href="/results">
+            <span className={styles.menuOptions}>Results</span>
+          </Link>
+          <Link href="/about">
+            <span className={styles.menuOptions}>About</span>
+          </Link>
+        </div>
       </div>
     </nav>
   );
