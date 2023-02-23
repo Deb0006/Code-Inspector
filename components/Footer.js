10690b151fab9e5a404ad0ae30ed24084db0dbf2
Debby <34969423+Deb0006@users.noreply.github.com>
2023-02-22 20:58:47 -0500

    Adds icons to Footer with css

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,35 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.wrapper}>
-        <p className={styles.copywrite}>Created by Debby A.</p>
+        <p className={styles.copyright}>Created by Debby A.</p>
+        <div className={styles.container}>
+          <a href="https://debby.dev/" target="_blank">
+            <div className={styles.link}>
+              <svg
+                width="18"
+                height="18"
+                fill="currentColor"
+                className={styles.portfolio}
+                viewBox="0 0 16 16"
+              >
+                <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11z" />
+              </svg>
+            </div>
+          </a>
+          <a href="https://github.com/Deb0006/Code-Inspector" target="_blank">
+            <div className={styles.link}>
+              <svg
+                width="18"
+                height="18"
+                fill="currentColor"
+                className={styles.github}
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+            </div>
+          </a>
+        </div>
       </div>
     </footer>
   );
